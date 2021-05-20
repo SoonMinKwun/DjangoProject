7381cb0220556e032274906667e34057b8726d86
SoonMinKwun <52186166+SoonMinKwun@users.noreply.github.com>
2021-05-21 00:30:38 +0900

    DB에 값 넣기 및 업데이트 완료
    DB에 값 넣기 및 업데이트 완료

--- stock_init.py
+++ stock_init.py
@@ -29,4 +29,37 @@ def confirm_emotion():
     inDB = list(mycol.find())
     return inDB
 
-# DB 값과 최신 크롤링 값 비교
+# DB연결
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["Stock_Emo_DB"]
+mycol = mydb["Stock_Emo_App_stock"]
+
+# DB 값과 최신 크롤링 값을 데이터프레임으로 비교
+DB = list(mycol.find())
+latest = latest_emotion()
+
+df1 = pd.DataFrame(DB)
+df2 = pd.DataFrame(latest)
+
+# DB의 feeling과 최신 크롤링한 feeling이 같은 것만 데이터프레임으로 추출
+latest_df = df2[df1['feeling'] != df2['feeling']]
+
+# 추출된 데이터프레임을 딕셔너리로 변환
+latest_dict = dict(zip(latest_df['stock'],latest_df['feeling']))
+'''
+latest_dict는 아래처럼 리턴
+{'AAPL': 'joy', 'GOOG': 'joy', 'GOOGL': 'joy', 'TSLA': 'sadness', 'FB': 'joy', 'TSM': 'sadness', 'V': 'anger', 'JNJ': 'disgust', 'JPM': 'guilt', 'DIS': 'sadness', 'HD': 'sadness', 'PYPL': 'fear', 'BAC': 'joy'}
+'''
+
+# 추출된 latest_dict를 리스트 안에 담기
+latest_list = [latest_dict]
+print(latest_list)
+print(type(latest_list))
+print(df2)
+
+# 추출된 latest_list(종목명, 감정, 코멘트)를 DB에 삽입
+for i in latest_list:
+    for stock, feeling in i.items():
+        is_changed = df2['stock'] == stock  # 종목명이 같으면
+        comments = df2[is_changed]['comments'].tolist()[0]  # 종목명이 같은 행의 comments 열만 추출해서 리스트 안에 담기(이중 리스트라서 [0] 붙힘)
+        mycol.update_one({'stock' : stock} , {'$set' : {'feeling' : feeling , 'comments' : comments}})
