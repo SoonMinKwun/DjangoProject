6bc74cc9f095751f6e312652adfac92ac371de33
SoonMinKwun <52186166+SoonMinKwun@users.noreply.github.com>
2021-05-24 09:29:25 +0900

    Re create django project
    Re create django project

--- Stock_Emo/Stock_Emo_App/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# admin 추가
-admin.site.register(Stock)
-admin.site.register(Member)
--- Stock_Emo/Stock_Emo_App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockEmoAppConfig(AppConfig):
-    name = 'Stock_Emo_App'
